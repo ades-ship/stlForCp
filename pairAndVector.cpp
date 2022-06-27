dc826fad8fa3c431950752b95f87852bb8cf9dd8
Adesh Sengar <adeshsengar156@gmail.com>
2022-06-27 22:18:43 +0530

    first commit

---
+++ pairAndVector.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int ,string> p;
+    p=make_pair(1,"adesh");
+    // or
+    p={1,"nitish"};
+    cout<<p.first<<" "<<p.second<<endl;
+    //   or
+    cout<<p.first<<" "<<p.second<<endl;
+    
+    return 0;
+}

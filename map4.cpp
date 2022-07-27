0a88f36f64a4ffaefef5e75a61465d1184b00bf6
Adesh Sengar <adeshsengar156@gmail.com>
2022-07-27 23:14:13 +0530

    commit

--- map4.cpp
+++ map4.cpp
@@ -1,63 +1,74 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void print(map<int,string> &m) {
-    cout<<"\n"<<m.size();
-    for(auto &it:m) {
-        cout<<it.first<<" "<<it.second<<endl;
+void print(map<int, string> &m)
+{
+    cout << "\n"
+         << m.size();
+    for (auto &it : m)
+    {
+        cout << it.first << " " << it.second << endl;
     }
 }
-int main() {
+int main()
+{
     // map is implement through red black  tree;
     //  map is used to store keys and values pair;
     // keys are stored in sorted ordered;
     //  for insert and access in maps complexity is O(log n) for each insertion and access of key-value pair O(log(n))
     //  for n key values pair is O(nlog(n));
-    //  
-    map<int,string> m;
-    m[1]="adesh";   // takes o(log n) time
-    m[4]="nitish";
-    m[2]="dhruv";
-    m.insert({3,"arinjay"});
+    // we can only take primitive datatype as well complex container datatype in case of maps like vector,pair,sets etc;
+
+    map<int, string> m;
+    m[1] = "adesh"; // takes o(log n) time
+    m[4] = "nitish";
+    m[2] = "dhruv";
+    m.insert({3, "arinjay"});
 
     // m[5] by default holds value empty string or 0 in case of int .
-    map<int ,string> :: iterator it;
-    for(it=m.begin(); it!=m.end(); it++) {
-        cout<<(*it).first<<" "<<(*it).second<<endl;
-    }
-    
-    for(auto it: m) {
-        cout<<it.first<<" "<<it.second<<endl;
+    map<int, string>::iterator it;
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        cout << (*it).first << " " << (*it).second << endl;
     }
 
-     auto gg=m.find(2);    //   O(log(n))
-     cout<<"-------------find---------->"<<endl;
-   
-     if(gg == m.end())
-     {
-        cout<<"no values";
-     }  else {
-          cout<<(*gg).first<<" "<<(*gg).second<<endl;
-     }
-    cout<<"\n --------------------maps values before erase------------->";
+    for (auto it : m)
+    {
+        cout << it.first << " " << it.second << endl;
+    }
+
+    auto gg = m.find(2); //   O(log(n))
+    cout << "-------------find---------->" << endl;
+
+    if (gg == m.end())
+    {
+        cout << "no values";
+    }
+    else
+    {
+        cout << (*gg).first << " " << (*gg).second << endl;
+    }
+    cout << "\n --------------------maps values before erase------------->";
     print(m);
-     if(gg!=m.end()) {
-        m.erase(gg);  // log(n)
-     }
- 
-    cout<<"\n --------------------maps values after erase------------->";
+    if (gg != m.end())
+    {
+        m.erase(gg); // log(n)
+    }
+
+    cout << "\n --------------------maps values after erase------------->";
     m.clear();
-    cout<<"\n ----------------after earse--------------->";
+    cout << "\n ----------------after earse--------------->";
 
     // auto it1=m.find(12);
     // cout<<(*it1++).first<<" "<<(*it1++).second<<endl;
- 
-    print(m);
- 
- cout<<"\n------------------------->>>>>>>>";
-    map<string,string> mp;
-    mp.insert({"adesh","geu"});    // time complexity when key is string   -->   s.size() * O(log(n))   s is string 
-    for(auto &i:mp) {
-        cout<<"\n"<<i.first<<" "<<i.second<<endl;
-    }
 
+    print(m);
+
+    cout << "\n------------------------->>>>>>>>";
+    map<string, string> mp;
+    mp.insert({"adesh", "geu"}); // time complexity when key is string   -->   s.size() * O(log(n))   s is string
+    for (auto &i : mp)
+    {
+        cout << "\n"
+             << i.first << " " << i.second << endl;
+    }
 }
--- set5.cpp
+++ set5.cpp
@@ -1,10 +1,12 @@
 // #include<bits/stdc++.h>
 // using namespace std;
-// void print(set<string> &st) 
+// void print(set<string> &st)
 // {
+//   ranged based loop
 //     // for(string &value: st) {
 //     //     cout<<value<<endl;
 //     // }
+
 //     // for(auto it:st)
 //     //  cout<<(*it)<<endl;
 
@@ -12,6 +14,11 @@
 //         cout<<(*it)<<endl;
 //      }
 // }
+
+//  set stores the unique keys only like maps did;
+//    implementation -----> red black trees;
+// we can only take primitive datatype as well complex container datatype in case of sets like vector,pair,sets etc;
+
 // int main() {
 //     set<string> st;
 //     st.insert("adesh");   // O(logn)
@@ -22,23 +29,21 @@
 //     if(it!=st.end()) {
 //         cout<<"find";
 //     }
-      
+
 //       cout<<"\n----------------------------->>>>>>>>>>>>>>>>>>>>>";
 //      if(it!=st.end()) {
 //         st.erase(it);
 //     }
-    
+
 //     print(st);
 
 // }
-
-
 
 // que
 
 // #include<bits/stdc++.h>
 // using namespace std;
-// void print(set<string> &st) 
+// void print(set<string> &st)
 // {   cout<<"\n-------------------------unique keys in set---------";
 //     // for(string &value: st) {
 //     //     cout<<value<<endl;
@@ -64,18 +69,18 @@
 //     cout<<it<<endl;
 //    }
 //    print(st);
-   
 
 // }
 
-
-
 // que on unordered_map
-
+// time complexity in accessing and find the elements is O(1);
+// we can only take primitive datatype in case of unordered set not complex container datatype like vector,pair,sets etc;
+// implementation ----------->  hash table
+// hash table is not defined got complex coatiners in case of unorered set. we can manuallsy define the hash table but it is quit costly;
 
 // #include<bits/stdc++.h>
 // using namespace std;
-// void print(set<string> &st) 
+// void print(set<string> &st)
 // {   cout<<"\n-------------------------unique keys in set---------";
 //     // for(string &value: st) {
 //     //     cout<<value<<endl;
@@ -97,7 +102,7 @@
 //           cin>>name;
 //           st.insert(name);    // o(1);
 //    }
-   
+
 //    cout<<"\n --------------------check whether string is present in set or not-----------";
 //         int q;
 //         cin>>q;
@@ -107,44 +112,45 @@
 //             if(st.find(chk)!=st.end()) {     //O(1) in case of find , insert in unordered_map
 //                 cout<<"yes"<<endl;
 //             }
-//             else 
+//             else
 //               cout<<"no"<<endl;
-            
-        
+
 //    }
-   
+
 // //    for(auto it : st) {
 // //     cout<<it<<endl;
 // //    }
 // //    print(st);
-   
 
 // }
 
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     //  it allows multiple keys;
+    // implementation---------> red black tree 
+    // multimap also uses red black tree for its implementation;
+    // we can use multisets in place of priority queue in graphs questions; 
+    // mostly all operation like access,erase,search are in log(n) complexity
     multiset<string> st;
-    st.insert("adesh");   // O(logn)
+    st.insert("adesh"); // O(logn)
     st.insert("nitish");
     st.insert("gaurav");
     st.insert("nitish");
 
-
-    auto it=st.find("adesh");   // O(logn)
-    if(it!=st.end()) {
-        cout<<"find";
+    auto it = st.find("adesh"); // O(logn)
+    if (it != st.end())
+    {
+        cout << "find";
     }
-      
-      cout<<"\n----------------------------->>>>>>>>>>>>>>>>>>>>>";
-     if(it!=st.end()) {
-        st.erase(it);
-    }
-    
-   
 
+    cout << "\n----------------------------->>>>>>>>>>>>>>>>>>>>>";
+    if (it != st.end())
+    {
+        st.erase(it);  // it can deleted on the iterator values not all the seame values 
+    }
+
+    st.erase("gaurav");   //  it will delete the values of gaurav 
 }
